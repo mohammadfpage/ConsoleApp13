d212a0e9dedfb31327624386ac6f660f70f4b517
mohammadfpage <fallahi.mq@gmail.com>
2025-11-05 11:11:41 -0800

    Add Product.cs class

---
+++ ConsoleApp13/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+    }
+}
