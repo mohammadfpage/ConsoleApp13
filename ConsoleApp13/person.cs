aa9e878107299bce921fc189e621136d122784a7
mohammadfpage <fallahi.mq@gmail.com>
2025-11-05 11:15:55 -0800

    Update person.cs

--- ConsoleApp13/person.cs
+++ ConsoleApp13/person.cs
@@ -10,8 +10,11 @@ namespace ConsoleApp13
     {
         public int Id { get; set; }
 
+        public string Email { get; set; }
+
         public string FullName { get; set; }
 
         public string Address { get; set; }
+
     }
 }
