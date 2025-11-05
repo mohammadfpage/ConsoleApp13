4f47d0fee80c00fe1e537c02af58d34fe319d850
mohammadfpage <fallahi.mq@gmail.com>
2025-11-05 10:55:25 -0800

    Add project files.

---
+++ ConsoleApp13/person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class person
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+    }
+}
